cfc0dd443866f91cd2a0896212903017d7212fb9
riyashekann <aninditaguha9@gmail.com>
2018-03-24 23:22:16 +0530

    gui updating

--- cipher_func.py
+++ cipher_func.py
@@ -32,3 +32,23 @@ def caesar_decrypt(ciphertext,key):
             answer_d=answer_d+value
         return (answer_d)
 
+
+def vigenere_encrypt(plaintext, key):
+    keylength = len(key)
+    key_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plaintext]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_int[i % keylength]) % 26
+        ciphertext += chr(value + 65)
+    return (ciphertext)
+
+def vigenere_decipher(ciphertext,key):
+    keylength = len(key)
+    key_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in ciphertext]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_int[i % keylength]) % 26
+        plaintext += chr(value + 65)
+    return (plaintext)
