85bcd773c74a7476836a82df4af58d64e3e5ca4b
riyashekann <aninditaguha9@gmail.com>
2018-03-24 21:33:17 +0530

    files added

---
+++ GUI.py
@@ -0,0 +1,119 @@
+__author__ = 'Anindita'
+import Tkinter as tk
+import cipher_func
+
+class main(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        def move_window(event):
+               self.geometry('+{0}+{1}'.format(event.x_root, event.y_root))
+
+        tk.Tk.__init__(self,*args,**kwargs)
+        container=tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        tk.Tk.title(self,"File Transfer")
+        tk.Tk.wm_overrideredirect(self,True)
+        tk.Tk.wm_resizable(self,0,0)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0,weight=1)
+        #tk.Tk.iconbitmap(self,default="aa.ico")
+        self.bind('<B1-Motion>', move_window)
+
+
+
+        def enterb(event):
+            btn_close.configure(text="X",background="#C60000")
+        def leaveb(event):
+            btn_close.configure(text="X",background="#04202C")
+
+
+        btn_close=tk.Button(self,width=3,text="X",relief="flat",bg="#04202c",fg="White",bd=0,activebackground="#777777",command=self.destroy,font=('Berlin Sans FB',15))
+        btn_close.bind("<Enter>",enterb)
+        btn_close.bind("<Leave>",leaveb)
+        btn_close.place(x=560,y=3)
+
+
+        self.frames = {}
+
+        for F in (Home,Encrypt_Frame,Decrypt_Frame):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(Home)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+        frame.winfo_toplevel().geometry("600x450")
+
+class Home(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+
+        w=tk.Canvas(self,width=1000,height=500,highlightthickness=0,background="#CCD1D9")
+        w.place(x=0,y=0)
+
+        Button_encrypt=tk.Button(self,text="Encrypt",font=("HELVETICA",12,"italic bold"),width=15,height=5,bd=0,relief="flat",background="#5BC8AC",fg="Black",command=lambda:controller.show_frame(Encrypt_Frame))
+        Button_encrypt.place(x=100,y=150)
+
+
+        Button_decrypt=tk.Button(self,text="Decrypt",font=("HELVETICA",12,"italic bold"),width=15,height=5,bd=0,relief="flat",background="#5BC8AC",fg="Black",command=lambda:controller.show_frame(Decrypt_Frame))
+        Button_decrypt.place(x=330,y=150)
+
+
+
+
+
+class Encrypt_Frame(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+
+
+        plaintext_label=tk.Label(self,text="Enter your plaintext here",width=20,height=1,font=("HELVETICA",12,"italic bold"),background="#5BC8AC",fg="Black")
+        plaintext_label.place(x=40,y=50)
+
+#This is the entry widget for taking the plaintext as input from user
+        plaintext_entry=tk.Entry(self,width=30)
+        plaintext_entry.place(x=280,y=50)
+
+#Error here in this function
+    # What I am trying to do is whenever the button named caesar is pressed it will come to this function and
+        #then it will get the value of the plaintext entered by user using  entry.get widget and then send the value to
+        # the actual function which is residing in the cipher_func file.. and from there it will fetch the encrypted text
+        #and print it in the Label area which has been mentioned below.
+        def Encipher_Caesar():
+            pt=plaintext_entry.get()
+            print(pt)
+            value=cipher_func.caesar_encrypt(pt)
+            Caesar_encryption_answer.configure(text=value)
+
+
+        Button_line=tk.Button(self,width=500,height=1,bd=0,relief="flat",state="disabled",background="black")
+        Button_line.place(x=0,y=80)
+
+
+        Button_caesar=tk.Button(self,text="Caesar",width=8,height=2,font=("HELVETICA",12,"italic bold"),bd=0,relief="flat",background="#5BC8AC",fg="Black",command=Encipher_Caesar)
+        Button_caesar.place(x=20,y=120)
+
+
+        Button_caesar=tk.Button(self,text="Playfair",width=8,height=2,font=("HELVETICA",12,"italic bold"),bd=0,relief="flat",background="#5BC8AC",fg="Black")
+        Button_caesar.place(x=130,y=120)
+
+#This is where the encrypted text will be placed
+        Caesar_encryption_answer=tk.Label(self,text=" ")
+        Caesar_encryption_answer.place(x=300,y=300)
+
+
+
+class Decrypt_Frame(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+
+
+app=main()
+app.mainloop()
+
---
+++ cipher_func.py
@@ -0,0 +1,37 @@
+__author__ = 'Anindita'
+import GUI
+#Caesar Cipher encryption and decryption
+def caesar_encrypt(plaintext):
+        #plaintext=raw_input("Insert your text").lower()
+
+        #key=int(raw_input("Enter the key value:"))
+        answer_e=''
+        for value in plaintext:
+            value=(ord(value)+3)
+            if value-3==32:
+                value=32
+            elif value>ord('z'):
+                value-=26
+            elif value<ord('a'):
+                value+=26
+            value=chr(value)
+            answer_e = answer_e +value
+        return(answer_e)
+
+def caesar_decrypt():
+        ciphertext=raw_input("Enter the cipher text").lower()
+        key_d=int(raw_input("Enter the key value:"))
+        answer_d=''
+        for value in ciphertext:
+            value=(ord(value)-key_d)
+            if value + key_d==32:
+                value = 32
+            elif value > ord('z'):
+                value-=26
+            elif value < ord('a'):                
+                value+=26
+            value=chr(value)
+            answer_d=answer_d+value
+        print(answer_d)
+
+#
