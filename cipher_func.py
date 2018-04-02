be1258cef9f222aede910fdc888b23138593699c
riyashekann <aninditaguha9@gmail.com>
2018-04-02 22:31:40 +0530

    function

--- cipher_func.py
+++ cipher_func.py
@@ -83,3 +83,100 @@ def OTP_decrypt(ciphertext, key):
     char = otp[cipher]
 
     return char + OTP_decrypt(ciphertext[1:], key[1:])
+
+def matrix(key):
+	matrix=[]
+	for e in key.upper():
+		if e not in matrix:
+			matrix.append(e)
+	alphabet="ABCDEFGHIKLMNOPQRSTUVWXYZ"
+
+	for e in alphabet:
+		if e not in matrix:
+			matrix.append(e)
+
+	#initialize a new list. Is there any elegant way to do that?
+	matrix_group=[]
+	for e in range(5):
+		matrix_group.append('')
+
+	#Break it into 5*5
+	matrix_group[0]=matrix[0:5]
+	matrix_group[1]=matrix[5:10]
+	matrix_group[2]=matrix[10:15]
+	matrix_group[3]=matrix[15:20]
+	matrix_group[4]=matrix[20:25]
+	return matrix_group
+
+def message_to_digraphs(message_original):
+	#Change it to Array. Because I want used insert() method
+	message=[]
+	for e in message_original:
+		message.append(e)
+
+	#Delet space
+	for unused in range(len(message)):
+		if " " in message:
+			message.remove(" ")
+
+	#If both letters are the same, add an "X" after the first letter.
+	i=0
+	for e in range(len(message)/2):
+		if message[i]==message[i+1]:
+			message.insert(i+1,'X')
+		i=i+2
+
+	#If it is odd digit, add an "X" at the end
+	if len(message)%2==1:
+		message.append("X")
+	#Grouping
+	i=0
+	new=[]
+	for x in xrange(1,len(message)/2+1):
+		new.append(message[i:i+2])
+		i=i+2
+	return new
+
+def find_position(key_matrix,letter):
+	x=y=0
+	for i in range(5):
+		for j in range(5):
+			if key_matrix[i][j]==letter:
+				x=i
+				y=j
+
+	return x,y
+
+def encrypt(message):
+	message=message_to_digraphs(message)
+	key_matrix=matrix(key)
+	cipher=[]
+	for e in message:
+		p1,q1=find_position(key_matrix,e[0])
+		p2,q2=find_position(key_matrix,e[1])
+		if p1==p2:
+			if q1==4:
+				q1=-1
+			if q2==4:
+				q2=-1
+			cipher.append(key_matrix[p1][q1+1])
+			cipher.append(key_matrix[p1][q2+1])
+		elif q1==q2:
+			if p1==4:
+				p1=-1;
+			if p2==4:
+				p2=-1;
+			cipher.append(key_matrix[p1+1][q1])
+			cipher.append(key_matrix[p2+1][q2])
+		else:
+			cipher.append(key_matrix[p1][q2])
+			cipher.append(key_matrix[p2][q1])
+	return cipher
+
+def cipher_to_digraphs(cipher):
+	i=0
+	new=[]
+	for x in range(len(cipher)/2):
+		new.append(cipher[i:i+2])
+		i=i+2
+	return new
